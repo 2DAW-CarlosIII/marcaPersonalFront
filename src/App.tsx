c677fad0f90ae8a2557ef7040b65eacb20a5c627
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-06-01 08:39:33 +0200

    Commit inicial
    Ejecución del comando `npx create-react-admin marcapersonal_front`
    
    las opciones elegidas han sido:
    
      * JSON Server
      * Hard coded username/password
      * posts y users como recursos
      * npm como gestor de paquetes
    
    Creado un fichero .gitignore con el siguiente contenido:
    /node_modules
    .env
    npm-debug.log
    yarn-error.log
    /.idea
    /.vscode
    
    creado el archivo .env.example ejecutando
    cp .env .env.example

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { dataProvider } from './dataProvider';
+import { authProvider } from './authProvider';
+
+export const App = () => (
+    <Admin
+        dataProvider={dataProvider}
+		authProvider={authProvider}
+	>
+        <Resource name="posts" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+		<Resource name="users" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+    </Admin>
+);
+
+    
