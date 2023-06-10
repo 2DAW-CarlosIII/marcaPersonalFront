258a7f015299d6d0845c7460fa38592567e16016
Carmen Márquez <carmenmquz@gmail.com>
2023-06-10 18:36:38 +0200

    Correcciones para visualizacion

--- src/Front/index.jsx
+++ src/Front/index.jsx
@@ -1,3 +1,13 @@
-export const Front = () => (
-<div>Welcome</div>
-);
+import NavbarComponent from "./components/common/Navbar"
+import LandingPage from "./components/pages/LandingPage"
+
+function Front() {
+    return (
+        <>
+            <NavbarComponent></NavbarComponent>
+            <LandingPage></LandingPage>
+        </>
+    )
+}
+
+export default Front
