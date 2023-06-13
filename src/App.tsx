237ddfd7a0d098eca661b8d24d29f8eb5bd1251b
Carmen Márquez <carmenmquz@gmail.com>
2023-06-13 22:50:46 +0200

    Vista proyectos creada

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import  Front  from './Front';
 import { RAdmin } from './RAdmin';
+import ProyectosView from './Front/components/pages/Proyectos';
 
 export const App = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Front />} />
             <Route path="/dashboard/*" element={<RAdmin />} />
+            <Route path="/proyectos" element={<ProyectosView/> } />
         </Routes>
     </BrowserRouter>
 );   
